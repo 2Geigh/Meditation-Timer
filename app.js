6acacebfd332186d2509c6eaf24d6011327c18b5
Nick Garcia <nicholasgarcia2003@protonmail.com>
2022-12-17 14:47:56 -0700

    input validity
    added a check on the validity of user inputs for the timer, as well as some bug fixes for the stop button.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var minutes = 1;
 var seconds = 1;
 var timeInSeconds = 0;
 
+var timerActive = false;
+
 
 function gong() {
     var audio = new Audio("gong.mp3");
@@ -15,6 +17,7 @@ function gong() {
 function drawSetup() {
 
     //Initialises variables
+    timerActive = false;
     hours = 1;
     minutes = 1;
     seconds = 1;
@@ -66,12 +69,13 @@ function drawSetup() {
     startButton.id = "startButton"
     //startButton.type = "submit";
     startButton.innerHTML = "Start";
-    startButton.onclick = drawCountdown;
+    startButton.onclick = checkTimeValidity;
     startButton.style.color = "red"; 
     document.getElementById("drawContainer").appendChild(startButton);
 }
 
-function drawCountdown() {
+function checkTimeValidity() {
+    var timeValidity = true; //assumes that the inputted time is valid to begin with proir to checking
 
     timeInSeconds = 0;
 
@@ -79,11 +83,60 @@ function drawCountdown() {
     minutes = parseInt(document.getElementById("minutes").value);
     seconds = parseInt(document.getElementById("seconds").value);
 
+    var timeInputValues = [hours, minutes, seconds];
+
+    //Checks for empty input fieds
+    if (isNaN(hours) == true) { hours = 0; }
+    if (isNaN(minutes) == true) { minutes = 0; }
+    if (isNaN(seconds) == true) { seconds = 0; }
+
     var hoursInSeconds = hours * 60 * 60; //converts hours to seconds
     var minutesInSeconds = minutes * 60; //converts minutes to seconds
 
     timeInSeconds = seconds + minutesInSeconds + hoursInSeconds; //the total summed time from the three input fields in seconds
     console.log(timeInSeconds);
+
+    //the following checks that the individual inputs are valid
+    for (let i = 0; i < timeInputValues.length; i++) {
+        if (isNaN(timeInputValues[i]) == true || timeInputValues[i] < 0) {
+            timeValidity = false;
+            alert("Please enter a valid time.");
+            drawSetup();
+            break;
+        }
+
+        //the following checks if the total time is valid
+        else if (timeInSeconds <= 0) {
+            timeValidity = false;
+            alert("Please enter a valid time.");
+            drawSetup();
+            break;
+        }
+    }
+
+    if (timeValidity == true) {
+        drawCountdown();
+    }
+}
+
+function drawCountdown() {
+/*
+    timeInSeconds = 0;
+
+    hours = parseInt(document.getElementById("hours").value);
+    minutes = parseInt(document.getElementById("minutes").value);
+    seconds = parseInt(document.getElementById("seconds").value);
+
+    //Checks for empty input fieds
+    if (isNaN(hours) == true) { hours = 0; }
+    if (isNaN(minutes) == true) { minutes = 0; }
+    if (isNaN(seconds) == true) { seconds = 0; }
+
+    var hoursInSeconds = hours * 60 * 60; //converts hours to seconds
+    var minutesInSeconds = minutes * 60; //converts minutes to seconds
+
+    timeInSeconds = seconds + minutesInSeconds + hoursInSeconds; //the total summed time from the three input fields in seconds
+    console.log(timeInSeconds);*/
 
     document.getElementById("drawContainer").innerHTML = ""; //Clears all content in #drawContainer to initialise it
     
@@ -105,11 +158,23 @@ function drawCountdown() {
     var stop = document.createElement("button");
     stop.id = "stopButton";
     stop.innerText = "Stop"
+    stop.onclick = stopTimerAndReset;
     document.getElementById("buttonFlexSpace").appendChild(stop);
 
     timeInSeconds = timeInSeconds - 1;
+    timerActive = true;
+    const timerInterval = setInterval(updateTimeDisplay, 1000);
 
-    setInterval(updateTimeDisplay, 1000);
+    function stopTimerAndReset() {
+        clearInterval(timerInterval);
+        drawSetup();
+    }
+
+    if (timeInSeconds == 0) {
+        timerActive = false;
+        timeInSeconds = 0;
+        clearInterval(timerInterval);
+    }
 }
 
 function updateTimeDisplay() {
